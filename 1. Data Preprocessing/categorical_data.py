11cc2e17bc3a884957a2375212c348cef24c1a8f
parekhjigar <hello@parekhjigar.com>
2020-01-31 20:19:35 +1100

    Added templates for data preprocessing

---
+++ 1. Data Preprocessing/categorical_data.py
@@ -0,0 +1,27 @@
+# Data Preprocessing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
+
+# Taking care of missing data
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values = np.nan , strategy = 'mean')
+imputer = imputer.fit(X[:,1:])
+X[:,1:] = imputer.transform(X[:,1:])
+
+# Encoding categorical data- One hot encoding(dummy variables)
+# Encoding the Independent Variable
+from sklearn.preprocessing import  OneHotEncoder
+from sklearn.compose import ColumnTransformer
+ct = ColumnTransformer([('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+X = np.array(ct.fit_transform(X), dtype=np.float)
+# Encoding Y data
+from sklearn.preprocessing import LabelEncoder
+y = LabelEncoder().fit_transform(y)
