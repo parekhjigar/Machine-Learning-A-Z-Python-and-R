208183b9454b0ceff39d3bbceeacf203b82abc71
parekhjigar <hello@parekhjigar.com>
2020-02-06 13:18:55 +1100

    Added decision tree regression in R

---
+++ 2. Regression/5. Decision Tree Regression/decision_tree_regression.R
@@ -0,0 +1,39 @@
+# Decision Tree Regression
+
+# Importing the dataset
+dataset = read.csv('Position_Salaries.csv')
+dataset = dataset[2:3]
+
+# Splitting the dataset into Training and Test set
+#install.packages('caTools')
+# library(caTools)
+# set.seed(123)
+# split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+# training_set = subset(dataset, split == TRUE)
+# test_set = subset(dataset, split == FALSE)
+
+# Feature scaling
+# training_set[, 2:3] = scale(training_set[, 2:3])
+# test_set[, 2:3] = scale(test_set[, 2:3])
+
+# Fitting Decision Tree Regression to the dataset
+# install.packages('rpart')
+library(rpart)
+regressor = rpart(formula = Salary ~ .,
+                  data = dataset,
+                  control = rpart.control(minsplit = 1))
+
+# Predicting a new result
+y_pred = predict(regressor, data.frame(Level = 6.5))
+
+# Visualising the Decision Tree Regression results (for higher resolution and smoother curve)
+library(ggplot2)
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.01)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = 'red') +
+  geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))),
+            colour = 'blue') +
+  ggtitle('Truth or Bluff (Regression Model)') +
+  xlab('Level') +
+  ylab('Salary') 
