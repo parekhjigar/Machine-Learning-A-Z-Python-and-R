80340ab5981b02dc9c60f1191944d078e908438e
parekhjigar <hello@parekhjigar.com>
2020-02-29 08:39:50 +1100

    Initialized UCB in python

---
+++ 6. Reinforcement Learning/1. Upper Confidence Bound (UCB)/upper_confidence_bound.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 29 07:29:15 2020
+
+@author: jigar
+"""
+
+# Upper Confidence Bound
+
+# Importing the libraries
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
