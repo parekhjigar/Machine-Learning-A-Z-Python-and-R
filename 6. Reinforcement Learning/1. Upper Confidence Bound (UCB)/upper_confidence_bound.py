6a9c629b21eae0d5726a38a97da11dd7f574239f
parekhjigar <hello@parekhjigar.com>
2020-03-01 22:41:09 +1100

    Implementing UCB

--- 6. Reinforcement Learning/1. Upper Confidence Bound (UCB)/upper_confidence_bound.py
+++ 6. Reinforcement Learning/1. Upper Confidence Bound (UCB)/upper_confidence_bound.py
@@ -16,3 +16,25 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
+
+# Implementing UCB
+import math
+N = 10000
+d = 10
+ads_selected = []
+number_of_selections = [0] * d
+sums_of_rewards = [0] * d
+for n in range(0, N):
+    ad = 0
+    max_upper_bound = 0
+    for i in range(0, d):
+        if (number_of_selections[i] > 0):        
+            average_reward = sums_of_rewards[i] / number_of_selections[i]
+            delta_i = math.sqrt(3/2 * math.log(n + 1) / number_of_selections[n])
+            upper_bound = average_reward + delta_i
+        else:
+            upper_bound = 1e400
+        if upper_bound > max_upper_bound:
+            max_upper_bound = upper_bound
+            ad = i
+        
