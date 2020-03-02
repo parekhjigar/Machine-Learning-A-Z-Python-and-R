71b2d8f3a9548aaccadfa4644418f80481a537c8
parekhjigar <hello@parekhjigar.com>
2020-03-02 14:15:28 +1100

    Updated UCB to visualize in Python

--- 6. Reinforcement Learning/1. Upper Confidence Bound (UCB)/upper_confidence_bound.py
+++ 6. Reinforcement Learning/1. Upper Confidence Bound (UCB)/upper_confidence_bound.py
@@ -46,3 +46,7 @@ for n in range(0, N):
     
 # Visualising the results
 plt.hist(ads_selected)
+plt.title("Histogram of ads selections")
+plt.xlabel("Ads")
+plt.ylabel("Number of times each ad was selected")
+plt.show()
